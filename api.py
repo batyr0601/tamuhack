a5751d9e5e7e25dd9eaaa16ed3bdabcd488e3a44
batyr0601 <batyr0601@gmail.com>
2023-01-29 03:40:13 -0600

    API for front-end

---
+++ api.py
@@ -0,0 +1,64 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+
+app = Flask(__name__)
+api = Api(app)
+
+class Users(Resource):
+    
+    # methods go here
+    def post(self):
+        parser = reqparse.RequestParser()  # initialize
+        
+        parser.add_argument('userId', required=True)  # add args
+        parser.add_argument('name', required=True)
+        parser.add_argument('city', required=True)
+        
+        args = parser.parse_args()  # parse arguments to dictionary
+        
+        # create new dataframe containing new values
+        new_data = pd.DataFrame({
+            'userId': args['userId'],
+            'name': args['name'],
+            'city': args['city'],
+            'locations': [[]]
+        })
+        # read our CSV
+        data = pd.read_csv('users.csv')
+        # add the newly provided values
+        data = data.append(new_data, ignore_index=True)
+        # save back to CSV
+        data.to_csv('users.csv', index=False)
+        return {'data': "glalglalglalga"}, 200  # return data with 200 OK
+
+    @app.route('/meow')
+    def get():
+        # data = pd.read_csv('users.csv')  # read CSV
+        data = data.to_dict()  # convert dataframe to dictionary
+        return {'data': "asl;kdfjasdf"}, 200  # return data and 200 OK code
+
+    pass
+
+    @app.route('/peepee/<name>')
+    def get1(name):
+        # return {'success': True, 'name': name}, 200
+        return '<div>' + name + '</div>'
+    
+    def post(name):
+        # return {'success': True, 'name': name}, 200
+        return '<div>' + name + '</div>'
+    
+class Locations(Resource):
+    # methods go here
+    pass
+    
+api.add_resource(Users, '/users')  # '/users' is our entry point for Users
+api.add_resource(Locations, '/locations')  # and '/locations' is our entry point for Locations
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)  # run our Flask app
+    app.add_url_rule('GET', '/users', get)
+
+
