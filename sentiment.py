0e874314edf0e212b04a242fc63b492e9640e9e2
Timur Takhtarov <timtaktamu@tamu.edu>
2023-01-29 03:57:41 -0600

    functions

--- sentiment.py
+++ sentiment.py
@@ -6,42 +6,46 @@ import risk
 import json
 import requests
 
-sid = SentimentIntensityAnalyzer()
+def get_sentiment():
+    sid = SentimentIntensityAnalyzer()
 
-keywords = ["economy", "gdp", "growth", "gain", "loss"]
-link = "https://seekingalpha.com/market-outlook/economy?page="
+    keywords = ["economy", "gdp", "growth", "gain", "loss"]
+    link = "https://seekingalpha.com/market-outlook/economy?page="
 
-scraper = webscraper.Scraper(keywords, link)
-titles = scraper.getTitles(10)
+    scraper = webscraper.Scraper(keywords, link)
+    titles = scraper.getTitles(10)
 
-average = []
+    average = []
 
-for header in titles:
-    average.append(sid.polarity_scores(header)['compound'] + 1)
+    for header in titles:
+        average.append(sid.polarity_scores(header)['compound'] + 1)
 
-total = 0
+    sentiment = (sum(average) / len(average)) - 1
 
-sentiment = (sum(average) / len(average)) - 1
+    return sentiment
 
-# print(sentiment)
-# print(scraper.getEconStats())
-# print(metrics.get_from_last_month())
+def get_econ_data():
+    scraper = webscraper.Scraper
+    econStats = scraper.getEconStats()
+    metrics = metrics.get_from_last_month()
 
-econStats = scraper.getEconStats()
-metrics = metrics.get_from_last_month()
+    return econStats, metrics
 
 tickers = ['AAPL', 'MSFT', 'AMZN']
 weights = [0.35, 0.35, 0.3]
 
-tickers, weights, data = risk.process_input(tickers, weights, 50)
+def get_risk_factor(tickers, weights, totalInvestment, weeks):
+    tickers, weights, data = risk.process_input(tickers, weights, weeks)
 
-var_array = risk.calculate_var(weights, data, 50, 100000)
+    sentiment = get_sentiment()
+    econStats, metrics = get_econ_data()
 
-productivity = float(econStats['Output per hour, nonfarm business, percent change from previous quarter at annual rate, seasonally adjusted'][1:4])
+    var_array = risk.calculate_var(weights, data, weeks, totalInvestment)
 
-# Input from API
-tickers = {}
-totalInvestment = 1000000
+    productivity = float(econStats['Output per hour, nonfarm business, percent change from previous quarter at annual rate, seasonally adjusted'][1:4])
 
-riskFactor = var_array[-1]/totalInvestment * abs(metrics[0])/2.5 * metrics[1]/3.5 * 100/(100-metrics[2]+5) * (100+productivity)/100 + sentiment/100
-print(riskFactor)
+    # Input from API
+    tickers = {}
+
+    riskFactor = var_array[-1]/totalInvestment * abs(metrics[0])/2.5 * metrics[1]/3.5 * 100/(100-metrics[2]+5) * (100+productivity)/100 + sentiment/100
+    print(riskFactor)
