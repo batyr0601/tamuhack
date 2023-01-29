845d725ffd9a3ecf3abf980072431646b3da71b5
Timur Takhtarov <timtaktamu@tamu.edu>
2023-01-29 04:22:43 -0600

    fjdslfd;lksa

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,9 @@
-import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import webscraper
+import yfinance as yf
+import pandas as pd
+import pandas_datareader.data as web
+import datetime as dt
 import metrics
 import risk
 import json
@@ -24,6 +27,25 @@ def get_sentiment():
 
     return sentiment
 
+def get_weights(tickers, quantities):
+    yf.pdr_override()
+    values = []
+    total = 0
+
+    for i in range(len(tickers)):
+        t = yf.Ticker(tickers[i])
+        price = t.info['bid'] 
+        value = price * quantities[i]
+        values.append(value)
+        total += value
+    
+    weights = []
+
+    for i in range(len(values)):
+        weights.append(values[i]/total)
+    
+    return weights
+
 def get_econ_data():
     scraper = webscraper.Scraper
     econStats = scraper.getEconStats()
@@ -45,4 +67,8 @@ def get_risk_factor(tickers, weights, totalInvestment, weeks):
     tickers = {}
 
     riskFactor = var_array[-1]/totalInvestment * abs(metrics[0])/2.5 * metrics[1]/3.5 * 100/(100-metrics[2]+5) * (100+productivity)/100 + sentiment/100
-    print(riskFactor)
+    return riskFactor
+
+tickers = ['AAPL', 'MSFT', 'AMZN', 'GOOG', 'FB']
+quantities = [1, 2, 16, 1, 3]
+print(get_weights(tickers, quantities))
